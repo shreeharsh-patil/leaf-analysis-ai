68f98ae6f2ca7d9503075f9130ffd838a379e99d
Տʜʀᴇᴇʜᴀʀꜱʜ <shreeharshgamerz@gmail.com>
2025-09-08 16:36:36 +0000

    remove  built with nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,9 +22,7 @@ export default function IntroPage() {
       </div>
        <footer className="absolute bottom-0 py-6 md:px-8 md:py-0">
         <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
-          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Built with Next.js and Genkit.
-          </p>
+          
         </div>
       </footer>
     </div>
